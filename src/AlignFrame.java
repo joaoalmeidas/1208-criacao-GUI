136f31ee0338f29f613bda14b405becf44b513af
joao <joao182cx@gmail.com>
2019-01-17 14:53:28 -0200

    Adição do dois JCheckBoxes que serão exibidos na tela

--- src/AlignFrame.java
+++ src/AlignFrame.java
@@ -2,6 +2,7 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,10 +13,13 @@ public class AlignFrame extends JFrame {
 	private final JButton botaoAjuda;
 	
 	private final JPanel botoes;
-	/*
 	
 	private final JCheckBox checkSnap;
 	private final JCheckBox checkShow;
+	
+	private final JPanel checks;
+	
+	/*
 	
 	private final JTextField textoX;
 	private final JTextField textoY;
@@ -25,6 +29,17 @@ public class AlignFrame extends JFrame {
 		
 		super("AlignFrame");
 		setLayout(new FlowLayout());
+		
+		checks = new JPanel();
+		checkSnap = new JCheckBox("Snap to Grid");
+		checkShow = new JCheckBox("Snap to Show");
+		
+		checks.setLayout(new GridLayout(2, 1, 5, 5));
+		
+		checks.add(checkSnap);
+		checks.add(checkShow);
+		
+		add(checks);
 		
 		botoes = new JPanel();
 		botaoOk = new JButton("Ok");
